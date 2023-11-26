7f3d8458f003f26e4ec16b84bd1272bb79470015
RandomUserRay <145318415+RandomUserRay@users.noreply.github.com>
2023-11-26 18:06:49 +0700

    Update AutoParry Blade.lua

--- AutoParry Blade.lua
+++ AutoParry Blade.lua
@@ -3,7 +3,7 @@ gui.ResetOnSpawn = false
 gui.Parent = game.CoreGui 
 
 local script = game:GetService("LocalScript")
-local FrameDrag = script
+local FrameDrag = script.DraggableFrame
 
 local frame = Instance.new("Frame")
 frame.Position = UDim2.new(0, 10, 0, 10)
