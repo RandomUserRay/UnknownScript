67c4c7dc6c3d94c6b5ae57d40b1083089728c4b3
muqh <145318415+RandomUserRay@users.noreply.github.com>
2024-06-11 20:49:48 +0700

    Create QPWhitelistCheck.lua

---
+++ QPWhitelistCheck.lua
@@ -0,0 +1,19 @@
+local http = game:GetService("HttpService")
+local plr = game:GetService("Players")
+local lplr = plr.LocalPlayer
+
+local succ, result = pcall(function()
+	return http:JSONDecode(game:HttpGet("https://raw.githubusercontent.com/RandomUserRay/UnknownScript/main/whitelistqp"))
+end)
+
+if succ and result[gethwid()] then
+    print("Whitelisted!")
+	
+	return
+end
+
+if not result[gethwid()] then
+     print("Not Whitelisted!")
+
+   return
+end
